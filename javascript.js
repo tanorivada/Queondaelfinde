8a2b932fa31085bbf2eb1436c9cdd6b55dfe72b1
Alexis Rivada <alexisrivada@gmail.com>
2023-11-16 18:22:36 -0300

    cambios de js

--- javascript.js
+++ javascript.js
@@ -1,83 +1,83 @@
 $(document).ready(function(){
     $(".catedral").hover(
-        function() {$(this).attr("src","Imagenes/catedral.jpg");},
-        function() {$(this).attr("src","Imagenes/historia catedral.jpg");
+        function() {$(this).attr("src","Imagenes/historia catedral.jpg");},
+        function() {$(this).attr("src","Imagenes/catedral.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".remo").hover(
-        function() {$(this).attr("src","Imagenes/remo.jpg");},
-        function() {$(this).attr("src","Imagenes/remo2.jpg");
+        function() {$(this).attr("src","Imagenes/remo2.jpg");},
+        function() {$(this).attr("src","Imagenes/remo.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".bicicleta").hover(
-        function() {$(this).attr("src","Imagenes/bicicleta.jpg.png");},
-        function() {$(this).attr("src","./Imagenes/bici2.jpg");
+        function() {$(this).attr("src","./Imagenes/bici2.jpg");},
+        function() {$(this).attr("src","Imagenes/bicicleta.jpg.png");
     });
 });
 
 $(document).ready(function(){
     $(".parques").hover(
-        function() {$(this).attr("src","Imagenes/parques.jpg");},
-        function() {$(this).attr("src","./Imagenes/parques2.jpg");
+        function() {$(this).attr("src","./Imagenes/parques2.jpg");},
+        function() {$(this).attr("src","Imagenes/parques.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".fuente").hover(
-        function() {$(this).attr("src","Imagenes/fuente.jpg");},
-        function() {$(this).attr("src","./Imagenes/costanera.jpg");
+        function() {$(this).attr("src","./Imagenes/costanera.jpg");},
+        function() {$(this).attr("src","Imagenes/fuente.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".mate").hover(
-        function() {$(this).attr("src","Imagenes/mates.jpg");},
-        function() {$(this).attr("src","Imagenes/mates2.jpg");
+        function() {$(this).attr("src","Imagenes/mates2.jpg");},
+        function() {$(this).attr("src","Imagenes/mates.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".restauran").hover(
-        function() {$(this).attr("src","Imagenes/restauran plaza.jpg");},
-        function() {$(this).attr("src","./Imagenes/restauran.jpg");
+        function() {$(this).attr("src","./Imagenes/restauran.jpg");},
+        function() {$(this).attr("src","Imagenes/restauran plaza.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".comida").hover(
-        function() {$(this).attr("src","Imagenes/asado.jpg");},
-        function() {$(this).attr("src","Imagenes/gastronomia.jpg");
+        function() {$(this).attr("src","Imagenes/gastronomia.jpg");},
+        function() {$(this).attr("src","Imagenes/asado.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".coctel").hover(
-        function() {$(this).attr("src","Imagenes/coctel.jpg");},
-        function() {$(this).attr("src","Imagenes/bares y pubs.jpg");
+        function() {$(this).attr("src","Imagenes/bares y pubs.jpg");},
+        function() {$(this).attr("src","Imagenes/coctel.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".boliche").hover(
-        function() {$(this).attr("src","Imagenes/boliche.jpg");},
-        function() {$(this).attr("src","./Imagenes/discotecas.jpg");
+        function() {$(this).attr("src","./Imagenes/discotecas.jpg");},
+        function() {$(this).attr("src","Imagenes/boliche.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".recitales").hover(
-        function() {$(this).attr("src","Imagenes/recitales.jpg");},
-        function() {$(this).attr("src","./Imagenes/recitales2.jpg");
+        function() {$(this).attr("src","./Imagenes/recitales2.jpg");},
+        function() {$(this).attr("src","Imagenes/recitales.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".ffd").hover(
-        function() {$(this).attr("src","Imagenes/fdd.jpg.png");},
-        function() {$(this).attr("src","Imagenes/ffd2.jpg");
+        function() {$(this).attr("src","Imagenes/ffd2.jpg");},
+        function() {$(this).attr("src","Imagenes/fdd.jpg.png");
     });
 });
